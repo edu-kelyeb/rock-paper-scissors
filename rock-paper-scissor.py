e6b0bd0f88f92fe19507d1a293da81b4f3a5329a
edu-kelyeb <kelvin.yeboah@skola.taby.se>
2025-11-17 15:01:02 +0100

    Lektion 3
    Vi började med att göra om lite i koden. Vi startade också på ett poängsystem. Därefter lade vi till en for och while loop för hur många spelomgångar. While-loopen kontrollerar att man har skrivit in en av de tre alternativen (sten, sax, påse).

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -1,8 +1,16 @@
 import random
-val=["sax","påse","sten"]
 
 print("välkommen till sten,sax,påse")
-print("skriv 'sten','sax'eller 'påse'")
+namn = input("skriv in ditt användarnamn: ")
 
-player= input(sten, sax, påse)
-int(input())
+val= ["sten","sax","påse"]
+poäng_spelare = 0
+poäng_dator = 0
+
+for runda in range(1, 4):
+    print(f"\n-- Runda {runda}--")
+    spelare_val = input("Vad väljer du? (Sten/Sax/Påse): ").lower()
+
+    while spelare_val not in val:
+        spelare_val= input("Felaktigt val! välj Sten, Sax eller Påse: ").lower()
+
