924ee61f7425eee508d08b2e0cc99e625604ba3a
edu-kelyeb <kelvin.yeboah@skola.taby.se>
2025-11-20 12:31:56 +0100

    lektion 4
    Idag har vi gjort reglerna för själva sten sax påse, villkoren för vem som vinner och så att den lägger till poäng när spelaren eller datorn vinner. Vi tog hjälp utav google för att hitta en liten grund ide för hur man skulle göra eftersom att vi tyckte att det var lite klurigt.

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -14,3 +14,17 @@ for runda in range(1, 4):
     while spelare_val not in val:
         spelare_val= input("Felaktigt val! välj Sten, Sax eller Påse: ").lower()
 
+    dator_val = random.choice(val)
+    print(f"Dator valde:{dator_val}")
+
+    if spelare_val == dator_val:
+        print("Oavgjort")
+    elif (spelare_val == "sten" and dator_val == "sax") or\
+        (spelare_val == "sax" and dator_val == "påse") or\
+        (spelare_val == "påse" and dator_val == "sten"):
+        print(f"{namn} vinner rundan !")
+        poäng_spelare += 1
+    else:
+        print("Datorn vinner rundan")
+        poäng_dator += 1
+
