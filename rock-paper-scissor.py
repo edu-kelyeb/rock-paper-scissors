955d44c56113a440f001cc2ba770730f9b523599
edu-kelyeb <kelvin.yeboah@skola.taby.se>
2025-11-24 14:44:48 +0100

    Update rock-paper-scissor.py
    Nu är vi färdiga med själva koden, vi avslutade med att bestämma slutresultatet. Först skriver koden ut hur mycket poäng vardera spelare fick, och bestämmer sedan med hjälp av ett villkor vem som hade mest poäng och där med vann spelet. Vi stötte på ett problem där användar namnet strulade  vi listade inte riktigt ut vad problemet var men det löste sig när vi bara prövade köra koden.

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -11,12 +11,14 @@ for runda in range(1, 4):
     print(f"\n-- Runda {runda}--")
     spelare_val = input("Vad väljer du? (Sten/Sax/Påse): ").lower()
 
+    # här kontrolleras giltigt val
     while spelare_val not in val:
         spelare_val= input("Felaktigt val! välj Sten, Sax eller Påse: ").lower()
 
     dator_val = random.choice(val)
     print(f"Dator valde:{dator_val}")
 
+    # På denna kod bestämmer vi en vinnare. 
     if spelare_val == dator_val:
         print("Oavgjort")
     elif (spelare_val == "sten" and dator_val == "sax") or\
@@ -28,3 +30,14 @@ for runda in range(1, 4):
         print("Datorn vinner rundan")
         poäng_dator += 1
 
+    #slutresultat
+print("\n-- slutresultat --")
+print(f"{namn}: {poäng_spelare} poäng")
+print(f"Datorn: {poäng_dator} poäng")
+
+if poäng_spelare > poäng_dator:
+    print(f"Grattis {namn}, du vann spelet!")
+elif poäng_spelare < poäng_dator:
+    print("Datorn vann spelet!")
+else:
+    print("spelet slutade oavgjort!")
